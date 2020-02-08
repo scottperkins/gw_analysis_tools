822ac3492555f4568f354bc48515606cb79a4b3e
Scott Perkins <scottep3@illinois.edu>
2020-02-08 00:07:25 -0600

    stupid plotting stuff

--- gwatpy/gwatpy_plot.py
+++ gwatpy/gwatpy_plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.style.use("gwat_rc")
+matplotlib.style.use("/Users/sperkins/git-repos/gw_analysis_tools/gwatpy/gwat_rc")
 matplotlib.rcParams["font.sans-serif"] = ["Apple Symbols","Microsoft Sans Serif","Tahoma"]
 
 def show_minor_grid():
