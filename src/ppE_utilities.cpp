d19c31b3c90a7da2feb68f842872d89174f7d294
Scott Perkins <scottep3@illinois.edu>
2020-05-15 21:53:44 -0500

    fixed an error

--- src/ppE_utilities.cpp
+++ src/ppE_utilities.cpp
@@ -20,6 +20,10 @@ bool check_mod(std::string generation_method)
 	{
 		return true;
 	}
+	if(generation_method.find("gIMR") != std::string::npos)
+	{
+		return true;
+	}
 	if(check_theory_support(generation_method)){
 		return true;
 	}
