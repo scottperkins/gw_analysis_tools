fixed issue with IMRPhenomPv2 -- phase offset had the wrong sign

tests/python/compare_responses.py
@@ -20,7 +20,7 @@ for i in np.arange(iterations):
     plt.loglog(data[0],data[1]*data[1]+data[2]*data[2],label="LAL")
     plt.loglog(data[0],data[3]*data[3]+data[4]*data[4],label="GWAT")
 #plt.legend()
-plt.xlim([100,8000])
+#plt.xlim([100,8000])
 
 #plt.ylim([10**-35,10**-55])
 
@@ -43,16 +43,16 @@ for i in np.arange(iterations):
 #plt.axvline(x=4901.91596895426, color='k')
 #plt.axvline(x=5882.29916274512, color='k')
 
-plt.ylim([10**-7,10**-5])
+#plt.ylim([10**-7,10**-5])
 plt.savefig("plots/diff_amp.pdf")
 plt.close()
 for i in np.arange(iterations):
     data = np.loadtxt("data/response_{}.csv".format(i),delimiter=',',unpack=True)
     tc, phic = calculate_constants(data[6], data[5],data[0])
-    data[5] += 10
-    data[6] += 10
-    data[6] += 2*np.pi*data[0] * tc
-    data[6] += phic
+    #data[5] += 10
+    #data[6] += 10
+    #data[6] += 2*np.pi*data[0] * tc
+    #data[6] += phic
 #    #plt.loglog(data[0],abs(data[1]-data[3])*2/(abs(data[1])+abs(data[3])))
 #    #plt.loglog(data[0],abs(data[2]-data[4])*2/(abs(data[2])+abs(data[4])))
 #    plt.loglog(data[0],abs(data[5]-data[6])*2/(abs(data[5])+abs(data[6])))
@@ -84,10 +84,10 @@ for i in np.arange(iterations):
     data = np.loadtxt("data/response_{}.csv".format(i),delimiter=',',unpack=True)
     
     tc, phic = calculate_constants(data[6], data[5],data[0])
-    data[5] += 10
-    data[6] += 10
-    data[6] += 2*np.pi*data[0] * tc
-    data[6] += phic
+    #data[5] += 10
+    #data[6] += 10
+    #data[6] += 2*np.pi*data[0] * tc
+    #data[6] += phic
 #    #plt.loglog(data[0],abs(data[1]-data[3])*2/(abs(data[1])+abs(data[3])))
 #    #plt.loglog(data[0],abs(data[2]-data[4])*2/(abs(data[2])+abs(data[4])))
 #    plt.loglog(data[0],abs(data[5]-data[6])*2/(abs(data[5])+abs(data[6])))
@@ -102,7 +102,7 @@ for i in np.arange(iterations):
 #plt.axvline(x=4897.83826041278, color='k')
 
 plt.legend()
-plt.xlim([0,8000])
+#plt.xlim([0,8000])
 
 #plt.xlim([1600,1700])
 plt.savefig("plots/comp_phase.pdf")

tests/python/multi_gaussian_bilby.py
@@ -9,21 +9,30 @@ outdir = "data/bilby/"
 bilby.utils.check_directory_exists_and_if_not_mkdir(outdir)
 
 mean1 = np.loadtxt("data/multi_gaussian_like_mean.csv",delimiter=',')
+dim = len(mean1)
 mean2 = -1*mean1
 covL = np.loadtxt("data/multi_gaussian_like_cov.csv",delimiter=',')
 likelihood = AnalyticalMultidimensionalBimodalCovariantGaussian(mean1,mean2,covL)
 
 
+#################################################
+## PRIOR ##
+#################################################
 
-mus = np.loadtxt("data/multi_gaussian_prior_mean.csv",delimiter=',')
-names = ["x{}".format(i) for i in np.arange(len(mus))]
-bounds = [(-10,10) for i in np.arange(len(mus))]
-cov = np.loadtxt("data/multi_gaussian_prior_cov.csv",delimiter=',')
-mvg = bilby.core.prior.MultivariateGaussianDist(names, nmodes=1, mus=mus, covs=cov,bounds=bounds)
+#mus = np.loadtxt("data/multi_gaussian_prior_mean.csv",delimiter=',')
+#names = ["x{}".format(i) for i in np.arange(dim)]
+#bounds = [(-10,10) for i in np.arange(dim)]
+#cov = np.loadtxt("data/multi_gaussian_prior_cov.csv",delimiter=',')
+#mvg = bilby.core.prior.MultivariateGaussianDist(names, nmodes=1, mus=mus, covs=cov,bounds=bounds)
+#
+#priors = dict()
+#for i in np.arange(dim):
+#    priors[names[i]] = bilby.core.prior.MultivariateGaussian(mvg,names[i]) 
 
-priors = dict()
-for i in np.arange(len(names)):
-    priors[names[i]] = bilby.core.prior.MultivariateGaussian(mvg,names[i]) 
+
+priors = bilby.core.prior.PriorDict()
+priors.update({"x{0}".format(i): bilby.core.prior.Uniform(-10, 10, "x{0}".format(i)) for i in range(dim)})
+#################################################
 
 if __name__ == "__main__":
     from multiprocessing import Pool
@@ -33,5 +42,5 @@ if __name__ == "__main__":
     #    outdir=outdir, label=label,pool=p)
 
     result = bilby.run_sampler(
-        likelihood=likelihood, priors=priors, sampler='ptemcee', nsamples = 1e3,nwalkers=200,threads=8,pos0='prior',ntemps=10,
+        likelihood=likelihood, priors=priors, sampler='ptemcee', nsamples = 1e3,nwalkers=20,threads=6,pos0='prior',ntemps=5,
         outdir=outdir, label=label)