973c9977dffff1fa9cea493d19ccdc9514c49713
Scott Perkins <scottep3@illinois.edu>
2022-05-25 11:18:40 -0500

    New structure for new mcmc_gw_tool_v2. Class structure for more priors ad priorData integration

---
+++ include/gwat/standardPriorLibrary.h
@@ -0,0 +1,69 @@
+#ifndef STANDARDPRIORLIBRARY_H
+#define STANDARDPRIORLIBRARY_H
+
+#include "util.h"
+#include <bayesship/bayesshipSampler.h>
+#include <bayesship/dataUtilities.h>
+
+struct priorData
+{
+	double **mod_priors;
+	double mass1_prior[2];
+	double mass2_prior[2];
+	double spin1_prior[2];
+	double spin2_prior[2];
+	double tidal1_prior[2];
+	double tidal2_prior[2];
+	double tidal_s_prior[2];
+	double EA_prior[6];
+	double RA_bounds[2];
+	double sinDEC_bounds[2];
+	bool tidal_love=true;
+	bool tidal_love_error;
+	bool alpha_param; 
+	double DL_prior[2];
+	double T_mcmc_gw_tool;
+	double T_merger; 
+	
+};
+
+bool tidal_love_boundary_violation(double q,double lambda_s);
+
+double EA_current_constraints(bayesship::positionInfo *position, int dim, priorData *PD);
+
+//Uniform in m1 and m2, transformed to lnM and eta
+double chirpmass_eta_jac(double chirpmass, double eta);
+
+//Uniform in m1 and m2, transformed to lnM and q
+double chirpmass_q_jac(double chirpmass, double q);
+
+//For use with spin aligned system 
+//-- mimics uniform magnitude and uniform cos \tilt, but mapped to chi
+//-- Fit in mathematica 
+double aligned_spin_prior(double chi);
+
+
+class logPriorStandard_D: public bayesship::probabilityFn
+{
+public:
+	priorData *PD=nullptr;
+	logPriorStandard_D(priorData *PD){this->PD = PD;};
+	virtual double eval(bayesship::positionInfo *position, int chainID);
+};
+
+class logPriorStandard_D_NRT: public logPriorStandard_D
+{
+public:
+	logPriorStandard_D_NRT(priorData *PD): logPriorStandard_D(PD){this->PD = PD;};
+	virtual double eval(bayesship::positionInfo *position, int chainID);
+};
+
+class logPriorStandard_D_NRT_EA: public logPriorStandard_D_NRT
+{
+public:
+	logPriorStandard_D_NRT_EA(priorData *PD): logPriorStandard_D_NRT(PD){this->PD = PD;};
+	virtual double eval(bayesship::positionInfo *position, int chainID);
+};
+
+
+#endif
