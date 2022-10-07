14b96c1e666c6c04a9480947e74271f7213e2c4a
Scott Perkins <scott.ellis.perkins@gmail.com>
2022-10-07 16:12:12 -0700

    Fixed commented out inclination angle waveform section and removed print statement in standard prior library file

--- src/waveform_generator.cpp
+++ src/waveform_generator.cpp
@@ -227,7 +227,7 @@ int fourier_waveform(T *frequencies, /**< double array of frequencies for the wa
 		/*! Handle the inclination angle dependence*/
 		/*! Iota is weird, because it's a extrinsic parameter, but one that has dependence on frequency (potentially)*/
 		/*! I think it should be handled separately because of complications like LISA response, etc*/
-	/*	for (int i =0 ; i < length; i++){
+		for (int i =0 ; i < length; i++){
 			wp->hcross[i] *= ci;
 			wp->hplus[i] *= std::complex<T>(.5,0) *(std::complex<T>(1,0)+ci*ci);
 		}
