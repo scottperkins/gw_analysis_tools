e6e1638893576d6c7735049aa05026c60bea1688
scottperkins <scott.ellis.perkins@gmail.com>
2023-01-24 23:35:03 +0000

    Deploying to gh-pages from  @ a36b91266efda2594ec97976d79d900aec08b271 🚀

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['logpriorstandard_5fd_0',['logPriorStandard_D',['../classlogPriorStandard__D.html',1,'']]],
-  ['logpriorstandard_5fd_5fnrt_1',['logPriorStandard_D_NRT',['../classlogPriorStandard__D__NRT.html',1,'']]],
-  ['logpriorstandard_5fd_5fnrt_5fea_2',['logPriorStandard_D_NRT_EA',['../classlogPriorStandard__D__NRT__EA.html',1,'']]],
-  ['logpriorstandard_5fd_5fnrt_5fmod_3',['logPriorStandard_D_NRT_mod',['../classlogPriorStandard__D__NRT__mod.html',1,'']]]
+  ['logpriorstandard_5fd_5fmod_1',['logPriorStandard_D_mod',['../classlogPriorStandard__D__mod.html',1,'']]],
+  ['logpriorstandard_5fd_5fnrt_2',['logPriorStandard_D_NRT',['../classlogPriorStandard__D__NRT.html',1,'']]],
+  ['logpriorstandard_5fd_5fnrt_5fea_3',['logPriorStandard_D_NRT_EA',['../classlogPriorStandard__D__NRT__EA.html',1,'']]],
+  ['logpriorstandard_5fd_5fnrt_5fmod_4',['logPriorStandard_D_NRT_mod',['../classlogPriorStandard__D__NRT__mod.html',1,'']]]
 ];
