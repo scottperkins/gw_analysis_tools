4ec771b86e995f7017368308a0de3a96f82198fa
Scott Perkins <scott.ellis.perkins@gmail.com>
2023-02-23 13:42:58 -0800

    Updated jupyter_docker_launch.sh to not duplicate the server (the image already automatically launches a server)

--- examples/injection_IMRPhenomD/jupyter_docker_launch.sh
+++ examples/injection_IMRPhenomD/jupyter_docker_launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run  \
-	-it \
         --platform linux/amd64 \
 	-p 8990:8989\
 	--rm \
@@ -13,5 +12,5 @@ docker run  \
         -v $(pwd)/python:/injection/python \
         -v $(pwd)/makefile:/injection/makefile \
         -v $(pwd)/jupyter_launch.sh:/injection/jupyter_launch.sh \
-	scottperkins/gwat.jupyter ./jupyter_launch.sh
+	scottperkins/gwat.jupyter 
 
--- examples/injection_IMRPhenomD/jupyter_launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-jupyter lab --no-browser --allow-root --port 8989 --ip 0.0.0.0 ./python/
