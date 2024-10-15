fcfe9c6e565844f1a22965e725373cfc221ff00f
Dhruv <dhruvsrivastava.2@gmail.com>
2024-10-15 11:21:27 -0500

    Test for varying Luminosity distance sys err and stat err

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -2,8 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
- set(CMAKE_INSTALL_PREFIX "/usr/local/lib")  
-#set(CMAKE_INSTALL_PREFIX "/opt/gw_analysis_tools/install")
+  set(CMAKE_INSTALL_PREFIX "/opt/gw_analysis_tools/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- tests/src/test_errors.cpp
+++ tests/src/test_errors.cpp
@@ -8,6 +8,7 @@
 #include "gwat/ppE_utilities.h"
 #include "gwat/error.h"
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <vector>
 
@@ -23,19 +24,19 @@ int main(int argc, char *argv[]){
 	//Create injection structure
 	gen_params params;	
 	//params.mass1 = 1.9 *MSOL_SEC;
-	params.mass1 = 2.0 *MSOL_SEC;
-	params.mass2 = 1.4 *MSOL_SEC;
-	params.spin1[2] = .001;
-	params.spin2[2] = .002 ;
-	params.Luminosity_Distance = 100;
-	params.incl_angle = 3*M_PI/4;
+	params.mass1 = 1.44;
+	params.mass2 = 1.29399;
+	params.spin1[2] = .003;
+	params.spin2[2] = -.002 ;
+	params.Luminosity_Distance = 63;
+	params.incl_angle = 2.532207345558998;
 
 	params.NSflag1 = true;
 	params.NSflag2 =true;
 
-	params.phiRef = .0;
-	params.RA = 1.;
-	params.DEC = 0.6;
+	params.phiRef = 2.;
+	params.RA = 3.42;
+	params.DEC = -.37;
 	params.f_ref = 20;
 	double chirpmass = calculate_chirpmass(params.mass1,params.mass2);//*MSOL_SEC;
 	double eta = calculate_eta(params.mass1,params.mass2);//*MSOL_SEC;
@@ -46,12 +47,14 @@ int main(int argc, char *argv[]){
 	params.dep_postmerger=true;
 	params.sky_average=false;
 	params.tidal_love=true;
-	params.tidal_s=200;
+	params.tidal_s=242;
 
 	double beta = 3.;
 	int b = -7.;
 	std::cout<<"Beta: "<<beta<<std::endl;
 	std::cout<<"b: "<<b<<std::endl;
+
+	
 	
 	params.Nmod = 1;
 	params.bppe = new double[1];
@@ -63,12 +66,15 @@ int main(int argc, char *argv[]){
 	params.appe = new double[1];
 	params.alphappe[0] = 0.;
 	params.appe[0] = -2.;
+
+	
 	
 	//params.tidal_love=false;
 	//params.tidal1=200;
 	//params.tidal2=100;
-	params.psi = 1.;
-	params.gmst = 2.;
+	params.psi = 2.;
+	double gps = 1187008882.4;
+	params.gmst = gps_to_GMST_radian(gps);
 	params.sky_average = false;
 
 	
@@ -76,7 +82,7 @@ int main(int argc, char *argv[]){
 	//double fmin = 5;
 	//double fmax = 2048;
 
-	double fmin = 5;
+	double fmin = 10;
 	double fmax = 2048;
 	
 	//double **psd = new double*[Ndetect];
@@ -88,8 +94,13 @@ int main(int argc, char *argv[]){
 	double T = 16;
 	//std::cout<<"TIME: "<<T/T_year<<std::endl;
 
-	params.tc = 3.*T/4.;
-	int length = 2000;
+	double Tsignal = 4;
+	//double Tsignal = 128; 
+	double deltaF = 1./Tsignal;
+	//Merger time -- 3/4 of total signal length. If using >4 seconds, change to Tsignal - 2
+	double T_merger= Tsignal*3./4.;
+	int length = (int)((fmax-fmin)/deltaF);
+	params.tc=Tsignal-T_merger;
 	double *frequency = new double[length];
 	int Ndetect = 3;
 	double **psd = new double*[Ndetect];
@@ -105,7 +116,7 @@ int main(int argc, char *argv[]){
 		}
 	}
 	else{
-		double deltaF = (fmax-fmin)/length;	
+		//double deltaF = (fmax-fmin)/length;	
 		for(int i = 0 ; i<length; i++){
 			frequency[i] = fmin + deltaF*i;
 		}
@@ -194,31 +205,82 @@ int main(int argc, char *argv[]){
 
 	double* output_sys = new double[dim];
 	double* output_stat = new double[dim];
+
+	
+
+	//Generate a plot of systematic error vs SNR, based on a range of luminosity distances
+
+	std::ofstream output_file("sys_err_data.csv");
+	std::ofstream stat_output_file("stat_err_data.csv");
+	std::ofstream waveform_output_file("waveform_data.csv");
+	std::ofstream bgr_waveform_output_file("bgr_waveform_data.csv");
+
+
+	
+	int no_of_DL_steps = 20;
+	int DL_step_size = 10.5;
+	int DLeval = 63;
+
+	double total_snr_temp = 0;
+
+	for(int a = 0; a < no_of_DL_steps; a++){
+	params.Luminosity_Distance = DLeval;
+	total_snr_temp = 0;
+
 	for(int i = 0 ; i<dim; i++){
 		output_sys[i] = 0;
 		output_stat[i] = 0;
 	}
+	for(int i = 0; i < Ndetect; i++){
+	total_snr_temp += pow_int( calculate_snr(SN[i],detectors[i],method, &params, frequency, length, "SIMPSONS", weights, false), 2);
+	}
 
-	//calculate_systematic_error(frequency, hcg, hcppE, length, method, detectors, detectors[0], output_sys, dim, &params, 2, psd[1]);
+	wp.hplus = hpg;	
+	wp.hcross = hcg;	
+	fourier_waveform(frequency, length, &wp, "IMRPhenomD",&params);
+	wp.hplus = hpppE;	
+	wp.hcross = hcppE;	
+	fourier_waveform(frequency, length, &wp, "ppE_IMRPhenomD_IMR",&params);
 
+	for(int i = 0; i < length; i++){
+		waveform_output_file<<frequency[i]<<","<<hcg[i]<<std::endl;
+		waveform_output_file<<frequency[i]<<","<<hcppE[i]<<std::endl;
+	}
+
+	calculate_systematic_error(frequency, hcg, hcppE, length, method, detectors, detectors[0], output_sys, dim, &params, 2, psd[1]);
+	calculate_statistical_error(frequency, length, method, detectors, detectors[0], output_stat, dim, &params, 2, psd);
+
+	output_file<<sqrt(total_snr_temp)<< ",";
+	stat_output_file<<sqrt(total_snr_temp)<< ",";
+	for(int i = 0; i < dim-1; i++){
+		output_file<<output_sys[i]<<",";
+		stat_output_file<<output_stat[i]<<",";
+	}
+	output_file<<output_sys[dim-1]<<std::endl;
+	stat_output_file<<output_stat[dim-1]<<std::endl;
+
+	DLeval+= DL_step_size;
+
+	}
+	output_file.close();
 	std::vector<std::string> param_info = {"RA", "DEC", "psi", "phiRef", "tc", "iota_L", "ln DL", "ln chirpmass", "eta", "chi1", "chi2"};
 
 	
 
 	for(int i = 0; i < dim; i++){
-	//std::cout<<__LINE__<<" ? "<< param_info[i]<<" - Systematic Error "<<i<<": "<<output_sys[i]<<std::endl;
+	std::cout<<__LINE__<<" ? "<< param_info[i]<<" - Systematic Error "<<i<<": "<<output_sys[i]<<std::endl;
 	}
 	
 
-	//calculate_statistical_error(frequency, length, method, detectors, detectors[0], output_stat, dim, &params, 2, psd);
+	calculate_statistical_error(frequency, length, method, detectors, detectors[0], output_stat, dim, &params, 2, psd);
 	//std::cout<<"SNR: "<<sqrt(output[0])<<std::endl;
 
 	for(int i = 0; i < dim; i++){
-	//std::cout<<param_info[i]<<"Statistical Error "<<i<<": "<<output_stat[i]<<std::endl;
+	std::cout<<param_info[i]<<" - Statistical Error "<<i<<": "<<output_stat[i]<<std::endl;
 	}
 
 	for(int i = 0; i < dim; i++){
-	//std::cout<<param_info[i]<<" - Systematic Error / Statistical Error "<<i<<": "<<output_sys[i]/output_stat[i]<<std::endl;
+	std::cout<<param_info[i]<<" - Systematic Error / Statistical Error "<<i<<": "<<output_sys[i]/output_stat[i]<<std::endl;
 	}
 
 	
